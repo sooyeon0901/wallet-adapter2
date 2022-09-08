30d6c46e8946815cbcb0c6444d5028affc51ec55
Sungho Park <mrchaos@naver.com>
2022-09-08 19:37:50 +0900

    0908

--- packages/wallets/cherry/src/adapter.ts
+++ packages/wallets/cherry/src/adapter.ts
@@ -1,21 +1,23 @@
-import { EventEmitter, SendTransactionOptions, WalletAccountError, WalletError, WalletName, WalletNotConnectedError, WalletPublicKeyError, WalletSendTransactionError, WalletSignTransactionError } from '@solana/wallet-adapter-base';
+import { EventEmitter, scopePollingDetectionStrategy, SendTransactionOptions, WalletAccountError, WalletError, WalletName, WalletNotConnectedError, WalletPublicKeyError, WalletSendTransactionError, WalletSignTransactionError } from '@solana/wallet-adapter-base';
 import { BaseWalletAdapter, WalletReadyState } from '@solana/wallet-adapter-base';
 import { clusterApiUrl, Connection, SendOptions, Transaction, TransactionSignature } from '@solana/web3.js';
 import { PublicKey, Signer, Keypair } from '@solana/web3.js';
 import bs58 from 'bs58';
-import { Sign } from 'crypto';
+import { Sign, timingSafeEqual } from 'crypto';
 import { SignatureKind } from 'typescript';
 import { sign } from 'tweetnacl';
 
 export const CherryWalletName = 'Cherry Wallet' as WalletName<'Cherry Wallet'>;
 
-const sleep = async (ms: number) => {
-    return new Promise((r) => setTimeout(r, ms));
-};
 
+// const cherryReceivePage = (e: any) => {
+//     return console.log('e======', e);
+// }
 export class CherryWalletAdapter extends BaseWalletAdapter {
-    private _publicKey: PublicKey | null = null;
-    private _autoApprove = false;
+    private _publicKey!: PublicKey | null;
+    protected _connecting!: boolean;
+    private _connected!: boolean;
+    //private _autoApprove = false;
 
     name = CherryWalletName;
     url = 'https://github.com/solana-labs/wallet-adapter#usage';
@@ -24,7 +26,15 @@ export class CherryWalletAdapter extends BaseWalletAdapter {
 
     constructor() {
         super();
-        this._autoApprove = true;
+        //this._autoApprove = true;
+        // scopePollingDetectionStrategy(() => {
+        //     if (this._connecting) {
+        //         this._readyState = WalletReadyState.Installed;
+        //         this.emit('readyStateChange', this._readyState);
+        //         return true;
+        //     }
+        //     return false;
+        // });
     }
 
     get connecting() {
@@ -36,64 +46,78 @@ export class CherryWalletAdapter extends BaseWalletAdapter {
     get readyState() {
         return WalletReadyState.Installed;
     }
-    get autoApprove(): boolean {
-        return this._autoApprove;
+    // get autoApprove(): boolean {
+    //     return this._autoApprove;
+    // }
+    get connected(): boolean {
+        //this.connected = true;
+        return this._connected;
     }
+
+    
+
     async connect(): Promise<void> {
-        var cherryAdres : string = "";
-        console.log("1");
-        //http://localhost:8080/public/member/loginUserPage
-        //http://localhost:8080/public/metaplex/cherryLogin
-        //http://localhost:8080/public/metaplex/cherryAdapterTest
-        fetch('http://localhost:8080/public/metaplex/cherryAdapterTest', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                userSn: "1000000000000001842"
-            }),
-            // req data 는 서버쪽에다가 작성 
-        })
-        .then((res) => res.json()) // 데이터 한번 정제해 줘야 함
-        .then((data) => {
-            if(data != 'undefined' && data != null){ // 랜더링 시 undefined 나오는거 해결 위함?
-                console.log('post/stringify서버에서 받은 데이터(뷰)==', JSON.stringify(data));
-                console.log('post서버에서 받은 데이터(뷰)==', data);
-                console.log('post지갑 주소(뷰)==', data.data);
-
-                cherryAdres = data.data;
-                console.log("2");
-                console.log('wallet 주입 후==', cherryAdres);
+        try {
+            console.log('connect 진입');
+            var cherryAdres : string = "";
+            this._connecting = true;
+            
+            if (this.connected || this.connecting) { return };
+            // if (! this._publicKey) { 
+            //     window.open("http://192.168.10.207:8080/public/metaplex/walletAdres", "_blank");
+            //     //return
+            // };
+            window.open("http://192.168.10.207:8080/public/metaplex/walletAdres", "_blank");
+            async function cherryReceivePage(e: any) {
+                console.log('=e ==', e);
+                if (e.origin == "http://localhost:8080" || e.origin == "http://192.168.10.207:8080") {
+                    console.log('==========================================================================');
+                    console.log('e======', e);
+                    console.log('e.data======', e.data.publicKey);
+                    
+                    return e.data.publicKey;
+                }
             }
-        }).then(async () => {
-            let buffer: Buffer;
-            try {
-                buffer = new PublicKey(cherryAdres).toBuffer(); 
-            } catch (error: any) {
-                throw new WalletAccountError(error?.message, error);
-            }
-            let publicKey: PublicKey;
-            try {
-                publicKey = new PublicKey(buffer);
-            } catch (error: any) {
-                throw new WalletPublicKeyError(error?.message, error);
-            }
-            this._publicKey = publicKey;
+            window.addEventListener('message', async (e) => {
+                console.log('이벤트 리스너 확인');
+                let buffer: Buffer;
+                let cherryAdres = await cherryReceivePage(e)
+                console.log('cherryAdres==', cherryAdres);
+                if(cherryAdres) {
+                    try {
+                        buffer = new PublicKey(cherryAdres).toBuffer(); 
+                        console.log('buffer==', buffer);
+                    } catch (error: any) {
+                        throw new WalletAccountError(error?.message, error);
+                    }
 
-            this.emit('connect', this._publicKey);
-            console.log("체리publicKey==", publicKey);
-            console.log("체리this._publicKey==", this._publicKey);
-            console.log("3");
+                    let publicKey: PublicKey;
+                    try {
+                        publicKey = new PublicKey(buffer);
+                        console.log('publicKey==', publicKey);
+                    } catch (error: any) {
+                        throw new WalletPublicKeyError(error?.message, error);
+                    }
 
-            //this.emit('connect', publicKey);//this.publicKey as PublicKey
-        })
-        .catch(err => {console.log(err)});
+                    this._publicKey = publicKey;
+                    this.emit('connect', this._publicKey);
+                }
+            }, false);
+            //this._connecting = false;
+            this._connected = true;
+
+        } catch (error: any) {
+            this.emit('error', error);
+            throw error;
+        } finally {
+            this._connecting = false;
+        }
     }
+    
 
     // 세션 끊어야하지 않으면 별다른 로직 추가할 필요는 없음
     async disconnect(): Promise<void> {
-        this._publicKey = null;
+        //this._publicKey = null; // 재 connect 시 null로 넘어가서 주석처리함. connect 및 disconnect 잘 작동함.
         this.emit('disconnect');
     }
 
